97a45222b1dbf03d4fe31a33abd1b27d15096a99
joelrowley <144295926+joelrowley@users.noreply.github.com>
2025-09-23 11:29:23 -0700

    Update Map.jsx

--- components/Map.jsx
+++ components/Map.jsx
@@ -60,8 +60,8 @@ const RegionsDisplay = () => {
         /* Responsive H1 that matches global site typography */
         .title {
           text-align: center;
-          font-weight: 400;
-          font-size: clamp(2.25rem, 4vw, 2.8rem);
+          font-weight: 500;
+          font-size: clamp(2.25rem, 4vw, 3.3rem);
           line-height: 1.1;
           margin: 0 0 clamp(1.5rem, 3vw, 2.5rem);
         }
